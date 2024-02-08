a803987d01cecccd7a502f6d58004a96d54330c4
iShqfty <alaiwatkareem@gmail.com>
2024-02-07 20:48:51 -0500

    Added IL2CPP index + more connections

---
+++ il2cpp/il2cppfunctional.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("documentationList").style.width = "25%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("documentationList").style.width = "0%";
+  }
