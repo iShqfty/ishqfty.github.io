2d1a333162575717e77a607ee60af042859b7e95
iShqfty <alaiwatkareem@gmail.com>
2024-02-10 21:42:15 -0500

    complete reformat

--- functional.js
+++ functional.js
@@ -1,8 +1,10 @@
 function openNav() {
+    document.getElementById("back").style.width = "100%";
     document.getElementById("myNav").style.width = "50%";
   }
   
   /* Close when someone clicks on the "x" symbol inside the overlay */
   function closeNav() {
+    document.getElementById("back").style.width = "0%";
     document.getElementById("myNav").style.width = "0%";
   }
--- il2cpp/il2cppfunctional.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("documentationList").style.width = "25%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("documentationList").style.width = "0%";
-  }
