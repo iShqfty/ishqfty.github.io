a6b5d6606034da61cd6723f9bd6e326bbeee8698
iShqfty <alaiwatkareem@gmail.com>
2024-02-11 10:29:37 -0500

    improved navbar + sidebar navigation

--- functional.js
+++ functional.js
@@ -1,10 +1,70 @@
 function openNav() {
     document.getElementById("back").style.width = "100%";
-    document.getElementById("myNav").style.width = "50%";
+    document.getElementById("toExpand").style.width = "50%";
   }
   
   /* Close when someone clicks on the "x" symbol inside the overlay */
   function closeNav() {
     document.getElementById("back").style.width = "0%";
-    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("toExpand").style.width = "0%";
+  }
+
+  let WikiOpen = false;
+
+  function toggleWiki() {
+    WikiOpen = !WikiOpen;
+    if (WikiOpen) {
+    document.getElementById("wiki_dropdown").style.height = "95px";
+    }
+    else {
+    document.getElementById("wiki_dropdown").style.height = "0px";
+    }
+  }
+
+  let AboutOpen = false;
+
+  function toggleAbout() {
+    AboutOpen = !AboutOpen;
+    if (AboutOpen) {
+    document.getElementById("about_dropdown").style.height = "95px";
+    }
+    else {
+    document.getElementById("about_dropdown").style.height = "0px";
+    }
+  }
+
+  let VideosOpen = false;
+
+  function toggleVideos() {
+    VideosOpen = !VideosOpen;
+    if (VideosOpen) {
+    document.getElementById("videos_dropdown").style.height = "95px";
+    }
+    else {
+    document.getElementById("videos_dropdown").style.height = "0px";
+    }
+  }
+
+  let ProjectsOpen = false;
+
+  function toggleProjects() {
+    ProjectsOpen = !ProjectsOpen;
+    if (ProjectsOpen) {
+    document.getElementById("projects_dropdown").style.height = "95px";
+    }
+    else {
+    document.getElementById("projects_dropdown").style.height = "0px";
+    }
+  }
+
+  let LeetCodeOpen = false;
+
+  function toggleLeetCode() {
+    LeetCodeOpen = !LeetCodeOpen;
+    if (LeetCodeOpen) {
+    document.getElementById("leetcode_dropdown").style.height = "90px";
+    }
+    else {
+    document.getElementById("leetcode_dropdown").style.height = "0px";
+    }
   }
